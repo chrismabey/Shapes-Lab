f25a02c4c0dbf1ece1bd381f1b49f622dfdd8fe0
chrismabey <chrismabey@users.noreply.github.com>
2016-01-27 08:53:22 -0500

    Update Lab14avst.java

--- src/Lab14avst.java
+++ src/Lab14avst.java
@@ -1,11 +1,23 @@
 import java.awt.*;
 import java.applet.*;
+import java.util.ArrayList;
+
 
 public class Lab14avst extends Applet
 {
+
    public void paint(Graphics g)
    {
-      drawGrid(g);
+	   	Shape square = new Shape1Square();
+		Shape triangle = new Shape2Triangle();
+		Shape octagon = new Shape3Octagon();
+		Shape circle = new Shape4Circle();
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		shapes.add(square);
+		shapes.add(triangle);
+		shapes.add(octagon);
+		shapes.add(circle);
+		drawGrid(g);
    }
 
    public void drawGrid(Graphics g)
