fbc68c9b9fb1dcc0caa19eaedc7cc73a8b43d2b7
chrismabey <chrismabey@users.noreply.github.com>
2016-01-30 20:03:59 -0500

    Update Shape2Triangle.java

--- src/Shape2Triangle.java
+++ src/Shape2Triangle.java
@@ -5,11 +5,11 @@ public class Shape2Triangle implements Shape
    public void drawShape(Graphics g)
    {
 	   Polygon triangle = new Polygon();
-	   triangle.addPoint(600,50);
-	   triangle.addPoint(700,175);
-	   triangle.addPoint(500,175);
+	   triangle.addPoint(600,60);
+	   triangle.addPoint(700,220);
+	   triangle.addPoint(500,220);
 	   g.setColor(Color.black);
-	   g.drawPolygon(triangle);
+	   g.fillPolygon(triangle);
 	   
    }
 }
