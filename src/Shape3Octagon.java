22d757980028ba758c0d75f8976814aa19134267
chrismabey <chrismabey@users.noreply.github.com>
2016-01-30 20:05:03 -0500

    Update Shape3Octagon.java

--- src/Shape3Octagon.java
+++ src/Shape3Octagon.java
@@ -1,20 +1,21 @@
+
 import java.awt.*;
 
 public class Shape3Octagon implements Shape
-{
+{	
    public void drawShape(Graphics g)
    {
 	   Polygon octagon = new Polygon();
-	   octagon.addPoint(50,100);
-	   octagon.addPoint(100,50);
-	   octagon.addPoint(250,50);
-	   octagon.addPoint(300,100);
-	   octagon.addPoint(300,200);
-	   octagon.addPoint(250,250);
-	   octagon.addPoint(100,250);
-	   octagon.addPoint(50,200);
+	   octagon.addPoint(160,360);
+	   octagon.addPoint(240,360);
+	   octagon.addPoint(280,400);
+	   octagon.addPoint(280,480);
+	   octagon.addPoint(240,520);
+	   octagon.addPoint(160,520);
+	   octagon.addPoint(120,480);
+	   octagon.addPoint(120,400);
 	   g.setColor(Color.black);
-	   g.drawPolygon(octagon);
+	   g.fillPolygon(octagon);
 	  
    }
 }
